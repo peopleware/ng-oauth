9fa00565fcd5fdca38a28e47c423152d5ecaa224
Glen Staes <glen_staes@peopleware.be>
2023-06-26 16:44:21 +0200

    Update Angular to version 15

--- ppwcode/ng-oauth/src/test.ts
+++ ppwcode/ng-oauth/src/test.ts
@@ -6,22 +6,7 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: {
-    context(
-        path: string,
-        deep?: boolean,
-        filter?: RegExp
-    ): {
-        keys(): Array<string>;
-        <T>(id: string): T;
-    };
-};
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
     teardown: { destroyAfterEach: false }
 });
-// Then we find all the tests.
-const context: { (id: string): unknown; keys(): Array<string> } = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
